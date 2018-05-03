067bbf1a7741eea8a8d1f3e772c9c72258de9dee
Cody Stamps <crstamps2@gmail.com>
2018-05-02 22:12:05 -0400

    Switch back to standard rails base.
    Because of the nature of devise and the salesloft omniauth
    it wasn’t possible to serve up the api due to the auth being needed
    without deleting a lot of boiler plate code.

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,5 @@ module SalesLoftIntegration
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.api_only = true
   end
 end
